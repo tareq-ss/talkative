0b3292476462ceb78a51f787a5975443a137b4e3
tareq <tareqmazen2004@gmail.com>
2025-07-07 03:55:50 -0700

    splash screen done and add screen utils

--- lib/featurs/splash/splash_screen.dart
+++ lib/featurs/splash/splash_screen.dart
@@ -7,6 +7,19 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(),
+          Image.asset("assets/images/Animation - 1751837012840.gif"),
+          Text("Taskati",style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 40
+          ),)
+        ],
+      ),
+    );
   }
 }
